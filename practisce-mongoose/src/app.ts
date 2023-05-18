b11327d4a67317c3d6f4b7cc8938f20dca26e790
Sumon <m.sumon@outlook.com>
2023-05-18 15:21:08 +0600

    first user interface and schema added

--- practisce-mongoose/src/app.ts
+++ practisce-mongoose/src/app.ts
@@ -1,11 +1,69 @@
-import express, { Application, Response, Request } from "express";
+import express, { Application, Response, Request, NextFunction } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
+import { strict } from "assert";
 
 const app: Application = express();
+
+//using cors
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+//parsing data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse any type data.
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  // res.send("Hello World!");
+  /*
+  step1: interface
+  step2: Schema
+  step3: model
+  step4: database query
+  */
+  // creating an interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNO: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+  // creating schema using interface
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: { type: String, required: true },
+    gender: { type: String, enum: ["male", "female"] },
+    email: { type: String },
+    contactNo: { type: String, required: true },
+    emergencyContactNO: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
 });
 
 export default app;
